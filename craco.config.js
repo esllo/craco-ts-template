a73356f2f0e26886e2563332620d5e6e12579629
esllo <master@esllo.com>
2022-03-02 22:57:24 +0900

    update config

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
     },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+    ],
+  },
+  babel: {
+    plugins: [
+      [
+        '@emotion/babel-plugin-jsx-pragmatic',
+        {
+          export: 'jsx',
+          import: '__to_avoid_prepending_/** @jsxImportSource @emotion/react */',
+          module: '@emotion/react',
+        },
+      ],
+      [
+        '@babel/plugin-transform-react-jsx',
+        {
+          pragma: '__to_avoid_prepending_/** @jsxImportSource @emotion/react */',
+        },
+      ],
+    ],
   },
   jest: {
     configure: {
